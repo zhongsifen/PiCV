da785ee0d546a0d0f5848f628a3234e9275e86f3
Sifen Zhong <zhongsifen@gmail.com>
2019-01-13 11:56:06 +0800

    py

--- ctpPiCV/PiCV.py
+++ ctpPiCV/PiCV.py
@@ -1,6 +1,5 @@
 from ctypes import *
-libPiCV = CDLL("/Users/zhongsifen/Work/PiCV/build/PiCV/libPiCV.dylib")
-path = "/Users/zhongsifen/Data/48.3gp"
+libPiCV = CDLL("/home/pi/Work/PiCV/build/PiCV/libPiCV.so")
+path = "/home/pi/Data/48.3gp"
 cpath = path.encode('utf-8')
-cpath = None
-libPiCV.run(cpath)
+libPiCV.run(None)
