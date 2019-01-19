c3358af66bfae76a2dab75389c965350b60f7b96
zhongsifen <zhongsifen@gmail.com>
2019-01-20 07:33:08 +0800

    cdll(path)

--- ctpPiCV/PiCV.py
+++ ctpPiCV/PiCV.py
@@ -1,5 +1,5 @@
-from ctypes import *
-libPiCV = CDLL("/home/pi/Work/PiCV/build/PiCV/libPiCV.so")
+import os
+libPiCV = CDLL(os.path.dirname(__file__) + "/../build/PiCV/libPiCV.so")
 path = "/home/pi/Data/48.3gp"
 cpath = path.encode('utf-8')
 libPiCV.run(None)
