a008b2ea305b2867ef57db5b1fe0aad2f0e4490a
zhongsifen <zhongsifen@gmail.com>
2019-03-10 17:12:49 +0800

    inden

--- PiCV/PiCV.hpp
+++ PiCV/PiCV.hpp
@@ -5,37 +5,44 @@
 #include <opencv2/imgcodecs.hpp>
 
 namespace PiCV {
-	typedef cv::Mat Image;
-	typedef cv::Mat_<uint8_t> Gray;
-	typedef cv::Rect Face;
-	typedef std::vector<cv::Point> Landmark;
-	typedef std::vector<cv::Point2f> EEM;
-	typedef cv::Mat Chip;
-	typedef std::vector<float> Desc;
-	
-	// const int _nfp =  68;	// number of feature points
-	// const int _ndv = 128;	// number of description values
-	
-	bool toGray(Image & image, Gray & gray);
-	bool toEEM(Landmark & landmark, EEM & eem);
-	bool toChipTri(Image & image, Landmark & landmark, EEM & tri, cv::Size & box, Chip & chip);
-	bool toMeasure(Desc & d1, Desc & d2, float & measure);
+// const int _nfp =  68;	// number of feature points
+// const int _ndv = 128;	// number of description values
 
-	void showFace(cv::Mat & img, Face & face);
-	void showLandmark(cv::Mat & img, Landmark & landmark);
-	void showEEM(cv::Mat & img, EEM & eem);
+typedef cv::Mat Image;
+typedef cv::Mat_<uint8_t> Gray;
+typedef cv::Rect Face;
+typedef std::vector<cv::Point> Landmark;
+typedef std::vector<cv::Point2f> EEM;
+typedef cv::Mat Chip;
+typedef std::vector<float> Desc;
 
-	extern "C"
-	{
-		bool setupVideo(char video_path[]);
-		bool readVideoFrame(Image & frame);
-		bool getVideoFrame(Image & frame);
-		bool showFrame(Image & frame);
-		bool showChip(Chip & chip);
-		bool drawFace(Image & frame, Face & face);
-		bool drawLandmark(Image &frame, Landmark &landmark);
+typedef struct {
+	Chip chip;
+	Desc desc;
+}
+Feat;
 
-		bool setup();
-		bool run();
-	}
+bool toGray(Image &image, Gray &gray);
+bool toEEM(Landmark &landmark, EEM &eem);
+bool toChipTri(Image &image, Landmark &landmark, EEM &tri, cv::Size &box, Chip &chip);
+bool toMeasure(Desc &d1, Desc &d2, float &measure);
+
+void showFace(cv::Mat &img, Face &face);
+void showLandmark(cv::Mat &img, Landmark &landmark);
+void showEEM(cv::Mat &img, EEM &eem);
+
+extern "C"
+{
+	bool setupVideo(char video_path[]);
+	bool readVideoFrame(Image &frame);
+	bool getVideoFrame(Image &frame);
+	bool showFrame(Image &frame);
+	bool showChip(Chip &chip);
+	bool drawFace(Image &frame, Face &face);
+	bool drawLandmark(Image &frame, Landmark &landmark);
+
+	bool setup();
+	bool run();
+}
+
 }
