6f0d01cc068c9d25a58d7ac65bf935faf11cb66f
zhongsifen <zhongsifen@gmail.com>
2019-08-13 01:25:08 +0800

    TooImage

--- PiKV/PiMien.py
+++ PiKV/PiMien.py
@@ -3,12 +3,20 @@ import kivy
 kivy.require('1.7.0')
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.widget import Widget
 from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.camera import Camera
 
 
 class PiMien(AnchorLayout):
-    pass
+    def PiPlay(self, instance):
+        self.ids._camera.play = True
+
+    def PiPause(self, instance):
+        self.ids._camera.play = False
+
+    def PiStop(self, instance):
+        self.ids._camera.stop()
 
 
 class PiMienApp(App):
